e0b493d4c99fe52a4610a173886cdfcdf10d60e1
Munthalalakshmi-tejaswi <22102A040176@mbu.asia>
2025-07-20 11:48:41 +0530

    ListCollection_Q4

---
+++ Collections/ListColl_Q4.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.*;
+
+public class ListColl_Q4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Number> num = new ArrayList<>();
+		num.add(100);
+		num.add(200.0);
+		for (Number n : num) {
+			System.out.println(n);
+		}
+	}
+
+}
