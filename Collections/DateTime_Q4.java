5a0854d26a67d90ecf8710916900e22c1c5f2b47
Munthalalakshmi-tejaswi <22102A040176@mbu.asia>
2025-07-24 23:29:24 +0530

    DateTime_Q4

---
+++ Collections/DateTime_Q4.java
@@ -0,0 +1,12 @@
+package collections;
+import java.time.*;
+public class DateTime_Q4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    LocalDate dt=LocalDate.of(2024,02,15);
+    System.out.println(dt.isLeapYear());
+  
+	}
+
+}
