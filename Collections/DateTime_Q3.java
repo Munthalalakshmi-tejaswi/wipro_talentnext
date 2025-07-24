4048dd40550aa63445900854f1e400a9dab4cc26
Munthalalakshmi-tejaswi <22102A040176@mbu.asia>
2025-07-24 23:28:08 +0530

    DateTime_Q3

---
+++ Collections/DateTime_Q3.java
@@ -0,0 +1,12 @@
+package collections;
+import java.time.*;
+public class DateTime_Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    LocalDate ld=LocalDate.of(0000,01,2);
+    System.out.println("Working in wipro :"+ld);
+    
+	}
+
+}
