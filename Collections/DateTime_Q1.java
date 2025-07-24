16f7766ab3e73f5ce855d426d9a5e7f1b17bcaef
Munthalalakshmi-tejaswi <22102A040176@mbu.asia>
2025-07-24 23:21:29 +0530

    DateTime_Q1

---
+++ Collections/DateTime_Q1.java
@@ -0,0 +1,13 @@
+package collections;
+import java.time.*;
+public class DateTime_Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     LocalDate ld=LocalDate.now();
+     System.out.println(ld);
+     LocalDate ld1=ld.plusDays(10);
+     System.out.println(ld1);
+	}
+
+}
