e6b23502e2cafa323734e27b07616ea3657a8175
Munthalalakshmi-tejaswi <22102A040176@mbu.asia>
2025-07-24 23:30:49 +0530

    DateTime_Q5

---
+++ Collections/DateTime_Q5.java
@@ -0,0 +1,12 @@
+package collections;
+import java.time.*;
+public class DateTime_Q5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    LocalTime lt=LocalTime.now();
+    System.out.println(lt);
+    System.out.println(lt.plusMinutes(25));
+	}
+
+}
